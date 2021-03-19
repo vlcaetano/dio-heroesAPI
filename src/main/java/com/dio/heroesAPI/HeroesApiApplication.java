85716b43bb3fa79e337c6da2e4cfea65f3d6eeb5
Vitor Caetano <vitorlc95@gmail.com>
2021-03-19 12:22:06 -0300

    first commit

---
+++ src/main/java/com/dio/heroesAPI/HeroesApiApplication.java
@@ -0,0 +1,13 @@
+package com.dio.heroesAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeroesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeroesApiApplication.class, args);
+	}
+
+}
